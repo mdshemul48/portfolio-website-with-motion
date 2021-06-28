3fff1ddd2ff7728e2437c156c6a96c1430f970cf
MD Shimul <mdshemul480@gmail.com>
2021-06-28 13:47:03 +0600

    adding comment to js code

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ gsap.registerPlugin(ScrollTrigger)
 
 const tl = gsap.timeline()
 
-
+// animating all the content
 tl.from(".content", {
     y: '-30%',
     opacity: 0,
@@ -12,6 +12,7 @@ tl.from(".content", {
 })
 
 
+// animating hero area 
 tl.from('.stagger1', {
     opacity: 0,
     y: -50,
@@ -25,9 +26,11 @@ tl.from(".hero-design", {
     opacity: 0, y: 50, ease: Power4.easeOut, duration: 1
 }, "-=2")
 
+// animating hero area design  
 gsap.from(".square-anim", {
     stagger: .2,
     scale: 0.1,
     duration: 1,
     ease: Back.easeOut.config(1.7)
 })
+
